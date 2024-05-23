839f82ed79bb3b642a77b4332d7d217c963a8cee
HenriqueHBM <henriquemadeira1@gmail.com>
2024-05-23 16:07:01 -0300

    ajuste sonar

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
                 </a>
                 <a href="https://www.instagram.com/henrique_bm_/" target="_blank">
                     <img src="./icons/instagram.png" alt="Instagram" class="links-width-margin">
-                </a> 
+                </a>
                 <a href="https://api.whatsapp.com/send?phone=5545998493946" target="_blank">
                     <img src="./icons/whatsapp.png" alt="Whatsapp" class="links-width-margin">
                 </a>
@@ -67,11 +67,10 @@
                 Nascido na data de 03 de Agosto de 2005, na cidade de Medianeira Paraná.
                 <br>
                 Comecei a focar mais na minnha carreira quando comecei a trabalhar na Frivatti Agro, onde estou atuando nos dias de hoje.
-                Na Frivatti, útilizamos o nosso sistema AgroSig, onde foi desenvolvido pelos desenvolvedores da mesma, o qual integra as linguagens: 
-                PHP, Javascript, Framework Laravel e sua linguagem de marcaçao de texto como: HTML e CSS, 
+                Na Frivatti, útilizamos o nosso sistema AgroSig, onde foi desenvolvido pelos desenvolvedores da mesma, o qual integra as linguagens:
+                PHP, Javascript, Framework Laravel e sua linguagem de marcaçao de texto como: HTML e CSS,
                 <br>
-                No dia a dia, sempre busco me aprimorar mais, descobrindo novas funções e linguangens para o aprimoramento pessoal.
-                
+                No dia a dia, sempre busco me aprimorar mais, descobrindo novas funções e linguangens para o aprimoramento pessoal.                
             </div>
         </div>
     </main>
