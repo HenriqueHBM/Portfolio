00ef94141883d84d1b4321a599abd1638f633510
HenriqueHBM <henriquemadeira1@gmail.com>
2024-05-30 17:07:04 -0300

    feat: parte de contato pronto

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Icone -->
     <link rel="icon" type="image/x-icon" href="./foto_vermelha.jpeg">
-    
+
     <title>| Henrique B. Madeira</title>
-    
+
     <!-- Font's -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,7 +17,10 @@
 
     <link rel="stylesheet" href="./style.css">
 
+    
+
 </head>
+
 <body>
     <header>
         <nav>
@@ -68,7 +72,7 @@
                 <br>
                 Comecei a focar mais na minnha carreira quando comecei a trabalhar na Frivatti Agro, onde estou atuando nos dias de hoje.
                 Na Frivatti, útilizamos o nosso sistema AgroSig, onde foi desenvolvido pelos desenvolvedores da mesma, o qual integra as linguagens:
-                PHP, Javascript, Framework Laravel e sua linguagem de marcaçao de texto como: HTML e CSS,
+                PHP, Javascript, Framework Laravel e sua linguagem de marcaçao de texto como: HTML e CSS.
                 <br>
                 No dia a dia, sempre busco me aprimorar mais, descobrindo novas funções e linguangens para o aprimoramento pessoal.
             </div>
@@ -76,17 +80,38 @@
     </main>
     <footer id="contato">
         <div class="text-lg">Contato</div>
-        <div id="contato-links">
+        <div id="contato-links" class="flex-divid">
             <div id="footer-email" class="text-sm">
-                Email: <a href="mailto:henrique.madeira1@gmail.com" target="_blank" class="clear-link">henrique.madeira@gmail.com</a>
+                Email: <a href="mailto:henrique.madeira1@gmail.com" target="_blank" class="clear-link">henriquemadeira03@gmail.com</a>
             </div>
             <div id="footer-telefone" class="text-sm">
-                Whatsapp: <a  href="https://api.whatsapp.com/send?phone=5545998493946" target="_blank" class="clear-link"> (+55) 45 998449-3946</a>
+                Whatsapp: <a href="https://api.whatsapp.com/send?phone=5545998493946" target="_blank" class="clear-link"> (+55) 45 998449-3946</a>
             </div>
+        </div>
+        <div id="contato-inputs" class="flex-divid">
+            <div class="field-group">
+                <input id="contato-nome" type="text" class="input-contato">
+                <label class="label-field" for="contato-nome">Nome</label>
+            </div>
+            <div class="field-group">
+                <input id="contato-email" type="text" class="input-contato" >
+                <label class="label-field" for="contato-email">Email</label>
+            </div>
+            <div class="field-group">
+                <textarea name="" id="" rows="4" class=" input-contato input-contato-textarea"></textarea>
+                <label class="label-field" for="contato-mensagem">Mensagem</label>
+            </div>
+            <div>
+                <button id="contato-botao" type="submit">
+                    Enviar
+                </button>
+            </div>
+
         </div>
     </footer>
     <div id="footer-copyright" class="text-sm">
         &copy; 2024 Henrique B. Madeira
     </div>
 </body>
+
 </html>
